1c7d625429e55adebd092df182660afade03e870
Colin <colin@probus.io>
2024-07-31 13:33:20 +0530

    UI changes

--- frontend/src/components/Qoutes.tsx
+++ frontend/src/components/Qoutes.tsx
@@ -2,14 +2,14 @@ export const Qoutes= () => {
     return <div className="bg-slate-300	h-screen flex flex-col justify-center">
         <div className="flex justify-center">
             <div className="max-w-md text-3xl font-bold justify-center">
-            {`"Hello, Welcome to my site testing the frontend"`}
+            {`"You can't connect the dots looking forward, you can only connect them looking backwards."`}
             </div>
         </div>
         <div className="max-w-lg text-l text-neutral-500 font-bold flex justify-center">
             Steve Jobs
         </div>
         <div className="max-w-lg text-l text-neutral-500 font-bold flex justify-center">
-            CEO Apple
+            Former CEO Apple
         </div>
     </div>
 }
